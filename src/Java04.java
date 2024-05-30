2040d960e1d1411cf6bafee9127024c99afad430
Sunryeo <elma9700@gmail.com>
2024-05-30 23:26:24 +0900

    prac: boolean

---
+++ src/Java04.java
@@ -0,0 +1,11 @@
+public class Java04 {
+  public static void main(String[] args) {
+    boolean isTrue = true;
+
+    if (isTrue) {
+      System.out.println("이건 참입니다.");
+    } else {
+      System.out.println("이건 거짓입니다.");
+    }
+  }
+}
