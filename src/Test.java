eefe17d5d77a16523e771a9f548cfbafbd115e2b
Sunryeo <elma9700@gmail.com>
2024-05-30 13:51:27 +0900

    prac: number data type

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,17 @@
 public class Test {
 
   public static void main(String[] args) {
-    int a = 30;
-    int b = 30;
-    int c = 40;
 
-    System.out.println(a + b + c);
+    // 정수형 자료형
+    int a = 30;
+    byte b = 2;
+    long c = 3;
+    short d = 4;
+
+    // 실수형 자료형
+    float e = 0.1f;
+    double f = 0.2;
+
+    String str = "Hello World";
   }
 }
