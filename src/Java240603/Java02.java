de08867ea57d759bc7e5efad3d5073336ab9aa52
Sunryeo <elma9700@gmail.com>
2024-06-03 22:43:59 +0900

    prac: logical operator

---
+++ src/Java240603/Java02.java
@@ -0,0 +1,17 @@
+package Java240603;
+
+public class Java02 {
+  public static void main(String[] args) {
+    System.out.println(true && true);
+    System.out.println(true && false);
+    System.out.println(false && true);
+    System.out.println(false && false);
+
+    System.out.println(true ^ true);
+    System.out.println(true ^ false);
+    System.out.println(false ^ true);
+    System.out.println(false ^ false);
+
+    System.out.println(0b01111 & 0b11110);
+  }
+}
