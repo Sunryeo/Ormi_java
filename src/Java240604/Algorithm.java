170cbc1785fc9651c739721766edba54a86341b6
Sunryeo <elma9700@gmail.com>
2024-06-05 21:39:23 +0900

    prac: algorithm practice

---
+++ src/Java240604/Algorithm.java
@@ -0,0 +1,14 @@
+package Java240604;
+
+public class Algorithm {
+  public static void main(String[] args) {
+    String str = "muzi frodo";
+    //        str.split(" ");
+    //    System.out.println(str.split(" ")[1]);
+    int c = 2;
+    //    System.out.println(Math.pow(c, 2));
+    Math.sqrt(c);
+    str = str.substring(0, str.length() - 1);
+    System.out.println(str);
+  }
+}
