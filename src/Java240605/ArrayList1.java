14f0d6c7bf022b10d1362043348f601853697a42
Sunryeo <elma9700@gmail.com>
2024-06-05 21:39:43 +0900

    prac: ArrayList

---
+++ src/Java240605/ArrayList1.java
@@ -0,0 +1,10 @@
+package Java240605;
+
+public class ArrayList1 {
+    public static void main(String[] args){
+        String[] arr = new String[2];
+        arr[0] = "A";
+        arr[1] = "B";
+        arr[2] = "C"; // 에러 발생
+    }
+}
