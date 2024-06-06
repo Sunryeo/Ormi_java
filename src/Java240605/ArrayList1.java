09f6f131069d1ddb3f4ab30cee2dfb9453536d7e
Sunryeo <elma9700@gmail.com>
2024-06-06 22:18:44 +0900

    prac: ArrayList

--- src/Java240605/ArrayList1.java
+++ src/Java240605/ArrayList1.java
@@ -1,10 +1,22 @@
 package Java240605;
 
+import java.util.ArrayList;
+
 public class ArrayList1 {
-    public static void main(String[] args){
-        String[] arr = new String[2];
-        arr[0] = "A";
-        arr[1] = "B";
-        arr[2] = "C"; // 에러 발생
-    }
+  public static void main(String[] args) {
+    String[] arr = new String[2];
+    arr[0] = "A";
+    arr[1] = "B";
+    //        arr[2] = "C"; // 에러 발생
+
+    ArrayList<Integer> al = new ArrayList<Integer>();
+    al.add(1);
+    al.add(2);
+
+    int value1 = al.get(0);
+    int value2 = al.get(1);
+
+    System.out.println(value1); // 1
+    System.out.println(value2); // 2
+  }
 }
