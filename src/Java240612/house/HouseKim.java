e4e7d04dbe79c50ecfc218229f1c2e0c96ad40e8
Sunryeo <elma9700@gmail.com>
2024-06-12 23:00:49 +0900

    prac: oop practice2

---
+++ src/Java240612/house/HouseKim.java
@@ -0,0 +1,7 @@
+package Java240612.house;
+
+public class HouseKim {
+    String lastName = "kim";
+    public String firstName = "seoungjo";
+    private String nickname = "jejumikkang";
+}
---
+++ src/Java240612/house/Test.java
@@ -0,0 +1,7 @@
+package Java240612.house;
+
+public class Test {
+    public static void main(String[] args){
+        new HouseKim();
+    }
+}
